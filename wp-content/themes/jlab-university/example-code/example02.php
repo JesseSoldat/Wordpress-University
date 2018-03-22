576bfdab6a145db9f3782267e3f317fccb17245d
Jesse <jsoldat@hotmail.com>
2018-03-22 21:01:14 +0700

    copy over static css and html and fix links

--- wp-content/themes/jlab-university/example-code/example02.php
+++ wp-content/themes/jlab-university/example-code/example02.php
@@ -1,3 +1,5 @@
+#3----------------------
+
 <?php
 $allSubjects = array('Math', 'English', 'Health', 'History', 'Science', 'Geography');
 
@@ -42,6 +44,19 @@ foreach ($failedSubjects as  $value) {
 }
 ?>
 </ul>
+
+
+
+#2----------------------
+<?php
+while(have_posts()) {
+  the_post(); ?>
+  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <p><?php the_content(); ?></p>
+  <hr>
+<?php 
+}
+?>
 
 
 
