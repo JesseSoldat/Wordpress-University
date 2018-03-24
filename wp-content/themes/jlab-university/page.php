7b28232242a1c218f184d0b98cd4462cd06d90e4
Jesse <jsoldat@hotmail.com>
2018-03-24 08:59:05 +0700

    page nav

--- wp-content/themes/jlab-university/page.php
+++ wp-content/themes/jlab-university/page.php
@@ -23,32 +23,60 @@ while(have_posts()) {
   <div class="container container--narrow page-section">
 
     <?php 
+      $theParent = wp_get_post_parent_id(get_the_ID());
       // echo get_the_ID(); //current page id
-      if(wp_get_post_parent_id(get_the_ID())) { ?>
-
+      if($theParent) { ?>
+      
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#">
+          <a class="metabox__blog-home-link" 
+            href="<?php echo get_permalink($theParent) ?>">
             <i class="fa fa-home" aria-hidden="true"></i> 
-            Back to About Us
+            <?php echo get_the_title($theParent); ?>
           </a> 
           <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
 
-    <?php }
-      
-    ?>
+    <?php }  ?>
 
-    
-    
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php 
+      //check if the current page has children
+      //if NO children return 0
+      $isAParent = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+
+      if ($theParent or $isAParent) { 
+    ?>
+    <div class="page-links">
+      <h2 class="page-links__title">
+        <a href="<?php echo get_permalink($theParent); ?>">
+          <?php 
+          //if it is the parent it will === 0 and wordpress with get the title for the current page
+            echo get_the_title($theParent); 
+          ?>
+        </a>
+      </h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php
+          //the current page has a parent
+          if($theParent) {
+            $findChildrenOf = $theParent;
+          } else {
+            //the current page has no parent so get its id
+            $findChildrenOf = get_the_ID();
+          }
+
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $findChildrenOf,
+            'sort_column' => 'menu_order'
+          ));
+        ?>
       </ul>
-    </div> -->
+    </div>
+    <?php } ?>
 
     <div class="generic-content">
       <?php the_content(); ?>
